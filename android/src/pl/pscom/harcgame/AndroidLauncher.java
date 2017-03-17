44c666cce9c132e58703e273ca369263595e48eb
PSCom <pscom@pscom.pl>
2017-03-17 02:45:25 +0100

    init project

---
+++ android/src/pl/pscom/harcgame/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package pl.pscom.harcgame;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import pl.pscom.harcgame.HarcGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new HarcGame(), config);
+	}
+}
---
+++ desktop/src/pl/pscom/harcgame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package pl.pscom.harcgame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import pl.pscom.harcgame.HarcGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new HarcGame(), config);
+	}
+}
